b0225f8bc3e99dd10ab14a20a3ab6f2b350995dd
y.congar <y.congar@EDS-YC2.local.local>
2013-11-11 12:06:39 +0100

    geändert von YC am 11.11.2013 um 12:06

--- src/start_gui/StartWindow.java
+++ src/start_gui/StartWindow.java
@@ -1,12 +1,141 @@
 package start_gui;
 
-import javax.swing.*;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
 
-public class StartWindow {
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class StartWindow implements ChangeListener {
+	private static final int breite = 300;
+	private static final int hoehe  = 300;
+	private static final int minSize=  15;
+	private static final int maxSize=  20;
+	private static final int steps  =   1;
+	private static final int startpos= 15;
+	private static final String filename = "Errlog.txt";
+	private FileWriter fstream ;
+	private JLabel sizeLbl;
+	private JLabel sliderLbl;
+	private JButton mybtn;
+	private Font font;
+	private Dimension dim;
+	
 	private JFrame frame;
-	private JComboBox width;
-	private JComboBox height;
+	private JPanel panel;
+	private GridLayout myLayout;
+	private JSlider slider;
+	private boolean created;
 	
+	public StartWindow(){
+		try{
+			//FileWriter und BufferedWriter wird erstellt um bestimmte Fehlermeldungen zu protokollieren
+			fstream = new FileWriter(filename);
+			
+			//Erstelle Button zum öffnen der MainWindow
+			mybtn = new JButton("Open");
+			
+			//Initialisiere alle Variablen für das Fenster
+			//sliderLbl => Zeigt an welche Größe man gewählt hat
+			sliderLbl = new JLabel("15x15");
+			
+			//sizeLbl => erklärt sich von selbst :)
+			sizeLbl = new JLabel("Wählen Sie eine Größe aus (Breite x Höhe) :");
+			
+			//frame => frame wird erstellt und bekommt eine Überschrift
+			frame = new JFrame("WALLS Startparameter");
+			
+			//panel für das frame wird erstellt
+			panel = new JPanel();
+			
+			//Ein Layout für das panel wird erstellt. In diesem Fall ein GridLayout()
+			//4 - > Rows, 1 -> Columns
+			myLayout = new GridLayout(4, 1);
+			
+			//Slider für die Auswahl der Größe wird erstellt.
+			//Minimum - Wert : 15, Maximum - Wert : 20;
+			slider = new JSlider(JSlider.HORIZONTAL, minSize, maxSize, startpos);
+			
+			//Eine Hilfsvariable dim (Dimension) wird erstellt.
+			dim = new Dimension(breite, hoehe);
+			
+			//Schriftart wurde gewählt und erstellt.
+			font = new Font("Tahoma", Font.ITALIC | Font.BOLD, 15);
+			
+			//Schriftart wird gesetzt.
+			sliderLbl.setFont(font);
+			sizeLbl.setFont(font);
+			slider.setFont(font);
+			
+			//Dem Slider wird ein Listener zugewiesen.
+			slider.addChangeListener(this);
+			
+			//Zahl wird in "steps"-Schitten angezeigt => Slider
+			slider.setMajorTickSpacing(steps);
+			
+			//Zeigt in "steps"-Schritten die einzelnen Punkte => Slider
+			slider.setMinorTickSpacing(steps);
+			
+			//Anzeige wird auf true gesetzt um die Zahlen und Striche anzuzeigen.
+			slider.setPaintTicks(true);
+			slider.setPaintLabels(true);
+			
+			
+			//Erklärt sich eigentlich auch von selbst :P
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			//Minimum Größe für panel und frame wird gesetzt
+			frame.setMinimumSize(dim);
+			panel.setMinimumSize(dim);
+			
+			//Bevorzugte Größe für panel und frame wird gesetzt
+			frame.setPreferredSize(dim);
+			panel.setPreferredSize(dim);
+			
+			//Komponenten werden dem Panel hinzugefügt.
+			panel.add(sizeLbl);
+			panel.add(slider);
+			panel.add(sliderLbl);
+			
+			//Panel wird dem frame übergeben.
+			frame.add(panel);
+			
+			//GridLayout wird dem panel übergeben
+			panel.setLayout(myLayout);
+			created = true;
+		}
+		catch(Exception e){
+			try {
+				Date time = new Date();
+				@SuppressWarnings("deprecation")
+				String strtime = time.toLocaleString();
+				fstream.append(e.getMessage()+"\t-->>\t"+strtime+"\n\r" );
+				fstream.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			created = false;
+		}
+		
+		
+	}
 	
+	public void stateChanged(ChangeEvent e) {
+	    JSlider source = (JSlider)e.getSource();
+	    if (!source.getValueIsAdjusting()) {
+	    	int size = source.getValue();
+	    	String forlbl = new String(size + "x" + size);
+	    	sliderLbl.setText(forlbl);
+	    }
+	}
 	
+	public void show(){
+		frame.setVisible(created);
+	}
 }
