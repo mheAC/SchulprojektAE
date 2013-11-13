e12bb673361619644e6a6062f42f6272bfc491f5
y.congar <y.congar@EDS-YC2.local.local>
2013-11-13 09:11:02 +0100

    JSquarePanel created by YC

---
+++ src/engine/JSquarePanel.java
@@ -0,0 +1,15 @@
+package engine;
+import javax.swing.*;
+
+public class JSquarePanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public JSquarePanel(){
+		
+	}
+
+}
