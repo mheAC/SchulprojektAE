219932d50e8df22b79e98af65c4d218b7e3598dd
y.congar <y.congar@EDS-YC2.local.local>
2013-11-13 09:23:58 +0100

    edited by YC

--- src/start_gui/JGameSquare.java
+++ src/start_gui/JGameSquare.java
@@ -1,6 +1,10 @@
 package start_gui;
 
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 import engine.SquareBase;
 
@@ -11,6 +15,12 @@ public class JGameSquare extends JPanel {
 	 */
 	private static final long serialVersionUID = 1L;
 	private SquareBase representingSquare;
+	private Border border;
+	
+	public JGameSquare(){
+		border = BorderFactory.createLineBorder(Color.black);
+		this.setBorder(border);
+	}
 	
 	public SquareBase getRepresentedSquare() {
 		return representingSquare;
