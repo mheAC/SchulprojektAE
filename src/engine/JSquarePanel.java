1ac699c4dfb314d3c9d8ef82600dac14a8335809
y.congar <y.congar@EDS-YC2.local.local>
2013-11-13 09:21:47 +0100

    edited by YC

--- src/engine/JSquarePanel.java
+++ src/engine/JSquarePanel.java
@@ -1,5 +1,8 @@
 package engine;
+import java.awt.Color;
+
 import javax.swing.*;
+import javax.swing.border.Border;
 
 public class JSquarePanel extends JPanel {
 
@@ -7,9 +10,11 @@ public class JSquarePanel extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private Border border;
 	
 	public JSquarePanel(){
-		
+		border = BorderFactory.createLineBorder(Color.black);
+		this.setBorder(border);
 	}
 
 }
