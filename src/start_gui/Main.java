8fdfab912ca6718d49eeaed6737223675f5654b2
y.congar <y.congar@EDS-YC2.local.local>
2013-11-13 11:07:04 +0100

    edited by YC

--- src/start_gui/Main.java
+++ src/start_gui/Main.java
@@ -1,7 +1,6 @@
 package start_gui;
 
 import java.awt.Component;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
