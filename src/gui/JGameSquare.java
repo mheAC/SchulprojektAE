af5ff86f4d427f02faa23ac1c40a1b08c6469f01
y.congar <y.congar@EDS-YC2.local.local>
2013-11-14 15:36:33 +0100

    yc

--- src/gui/JGameSquare.java
+++ src/gui/JGameSquare.java
@@ -16,14 +16,12 @@ public class JGameSquare extends JPanel {
 	
 	private SquareBase representingSquare;
 	private int position;
-	private Dimension dim;
 	
 	private Border border;
 	
 	public JGameSquare(){
 		super();
 		border = BorderFactory.createLineBorder(Color.black);
-		dim = getDimension();
 		this.setBorder(border);
 		
 		this.setPreferredSize(new Dimension(30,30)); // make any panel have a nice size
@@ -48,27 +46,18 @@ public class JGameSquare extends JPanel {
 		}
 	}
 	
-	private Dimension getDimension(){
-		if(this.dim == null)
-			this.dim = new Dimension(30,30);
-		
-		return this.dim;
-	}
-	
-	public void setDimension(Dimension di){
-		this.dim = di;
-	}
-	
 	private void drawVertikal(JGameSquare gs){
 		Graphics2D g = (Graphics2D)this.getGraphics();
+		Dimension dim = this.getSize();
 		g.setStroke(new BasicStroke(5));
-		g.drawLine(0, 15, 30, 15);
+		g.drawLine(0, dim.height/2, dim.width,  dim.height/2);
 	}
 	
 	private void drawHorizontal(JGameSquare gs){
 		Graphics2D g = (Graphics2D)this.getGraphics();
+		Dimension dim = this.getSize();
 		g.setStroke(new BasicStroke(5));
-		g.drawLine(15,0,15,30);
+		g.drawLine(dim.width/2,0,dim.width/2,dim.height);
 	}
 	public void setPosition(int position) {
 		this.position = position;
