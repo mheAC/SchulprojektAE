787f42bba838ba12b9f47a68f1ce93bac360877a
Michael Herpers <michael.herpers@googlemail.com>
2014-01-10 11:01:20 +0100

    .

---
+++ src/engine/loghandler/commands/Save.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ *@author Michael Herpers (michael.herpers@gmail.com)
+ */
+package engine.loghandler.commands;
+
+import engine.GameGrid;
+import engine.loghandler.receiver.LogHandler;
+
+/**
+ *
+ *
+ */
+public class Save implements Command{
+	
+	private LogHandler save;
+	
+	public Save(LogHandler saveobj){
+		this.save = saveobj;
+	}
+
+	/* (non-Javadoc)
+	 * @see engine.loghandler.commands.Command#execute()
+	 */
+	@Override
+	public void execute(GameGrid gg) {
+		this.save.setStack(gg);
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see engine.loghandler.commands.Command#undo()
+	 */
+	@Override
+	public void undo(GameGrid gg) {
+		this.save.delStack(gg);
+	}
+	
+}
